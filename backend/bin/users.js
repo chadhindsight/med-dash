1138faee255fc7f0c1cfd2e5dbfd353e24481977
Chad Hinds <chadrickj8@gmail.com>
2020-05-19 14:34:40 -0400

    Stuff

---
+++ backend/bin/users.js
@@ -0,0 +1,44 @@
+const users = [
+    {
+        email: 'gamer_girl69@gmail.com',
+        name: 'Samantha Jay',
+        primaryPharmacy: 'CVS',
+        medications: ["5ec403fb3585906a9c4aa04a", '5ec403fb3585906a9c4aa044', '5ec403fb3585906a9c4aa056'],
+    },
+    {
+        email: 'giovanni@gmail.com',
+        name: 'giovanni Totti',
+        primaryPharmacy: "Leeroy Pharmacy",
+        medications: ["5ec403fb3585906a9c4aa04a", '5ec403fb3585906a9c4aa044', '5ec403fb3585906a9c4aa056'],
+    },
+    {
+        email: 'weeeeeeeeeed@hotmail.com',
+        name: 'Steven A Smith',
+        primaryPharmacy: "Walgreens",
+        medications: ["5ec403fb3585906a9c4aa067", "5ec403fb3585906a9c4aa05f", '5ec403fb3585906a9c4aa056'],
+    },
+    {
+        email: 'DJceecee@hotmail.com',
+        name: 'Cirian Murphy',
+        primaryPharmacy: 'CVS',
+        medications: ["5ec403fb3585906a9c4aa04a", '5ec403fb3585906a9c4aa044', '5ec403fb3585906a9c4aa056'],
+    },
+    {
+        email: 'slayer500@gmail.com',
+        name: 'Carl Watson',
+        primaryPharmacy: "Duane Reade",
+        medications: ['5ec403fb3585906a9c4aa044', "5ec403fb3585906a9c4aa05d"]
+    },
+    {
+        email: 'DJceecee@hotmail.com',
+        name: 'Cirian Murphy',
+        primaryPharmacy: String,
+        medications: ["5ec403fb3585906a9c4aa04a", '5ec403fb3585906a9c4aa044', '5ec403fb3585906a9c4aa056'],
+    },
+    {
+        email: 'myFave@gmail.com',
+        name: 'Nicolle',
+        primaryPharmacy: String,
+        medications: ["5ec403fb3585906a9c4aa04a", "5ec403fb3585906a9c4aa072", "5ec403fb3585906a9c4aa07c"]
+    }
+]
