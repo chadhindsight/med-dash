c7dbb4eb839ca27de787f01f9421bbeacc5cfb72
Chad Hinds <chadrickj8@gmail.com>
2020-05-20 10:30:57 -0400

    Change medicine search to findOne instead of find

--- backend/routes/medicine.js
+++ backend/routes/medicine.js
@@ -3,25 +3,32 @@ const router = express.Router();
 const Medicine = require('../models/Medicine')
 const User = require('../models/User')
 
-// Seach for meds
-router.get('/medicine/search', (req, res, next) => {
-    Medicine.find({
-        name: { $regex: `[${req.body.name}]`, $options: "gi" }
+// Search for med & check if it is prescribed to the user currently logged in. Maybe combine with route on line 20
+router.get('/medicine/search/:name', isAuth, (req, res, next) => {
+    console.log(req.user)
 
-    }).then(n => {
-        res.json({ n })
-    })
+    let search = `".*${req.params.name}*."`
+    Medicine.findOne({
+         'drugName': { '$regex': req.params.name, '$options': 'i' } 
+
+    }).then(med => {
+        // Check if the id of med matches any ids prescribed to user
+        console.log(med)
+        User.find({medications:[med._id]})
+    }).catch(err => console.log('Medication not found!'))
 })
 
-// Select a specific med
 // Place your order, schedule delivery and update User orders in db
 router.get('/medicine/:id/order', (req, res) => {
     Medicine.findById(req.params.id).then(medicine => {
-        res.json({ medicine })
         // Check if that particular medicine id has been prescribed to User
         let presciptionList = User.find({medications: [medicine]})
+        
+        res.json({ medicine })
     })
 })
-
+function isAuth(req, res, next) {
+    req.isAuthenticated() ? next() : res.status(304).json({ msg: 'Log in first' });
+}
 // User.findByIdAndUpdate when order has been placed
 module.exports = router;
