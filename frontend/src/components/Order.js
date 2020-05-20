b74b445af1b2f26b9335d36d378cfeeb4198872a
Chad Hinds <chadrickj8@gmail.com>
2020-05-20 17:42:36 -0400

    Convert to stateless component

---
+++ frontend/src/components/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import actions from '../services/index'
+
+const Order = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Order;
