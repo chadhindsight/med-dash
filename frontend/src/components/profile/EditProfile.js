2a9b825da9da94176b64f0d0fa3491cee403d757
Chad Hinds <chadrickj8@gmail.com>
2020-05-22 11:20:55 -0400

    updated user profile edit logic to be more dynamic

--- frontend/src/components/profile/EditProfile.js
+++ frontend/src/components/profile/EditProfile.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import actions from '../../services/index'
 import InputEdit from './InputEdit';
 // Array of objects
@@ -6,48 +6,52 @@ import InputEdit from './InputEdit';
 let inputs = [
     {
         type: 'email',
-        name: 'email'
+        name: 'email',
+        disabled: true
     }, 
     {
         type: 'text',
-        name: 'name'
+        name: 'name',
+        disabled: true
     }, 
     {
         type: 'password',
-        name: 'password'
+        name: 'password',
+        disabled: true
     }, 
     // Find a pharmacy using maps maybe
     {
-        type: 'search',
-        name: 'primaryPharmacy'
+        type: 'text',
+        name: 'primaryPharmacy',
+        disabled: true
     }
 ]
 
-class EditProfile extends Component {
-    state = {
+function EditProfile (props) {
+    const [state, setState] = useState(inputs)
+    const [user, setUser] = useState(inputs.reduce((a,v) => (a[v.name]=props.user[v.name],a),{}))
 
-    }
-    handleSubmit = async e => {
-        e.preventDefault()
-        await actions.editProfile(this.state).then(res=>console.log(res.data))
-    }
-    displayInputField = () => {
-        
-    }
-
-    handleChange = (e) => {
-        this.setState({
+    const handleChange = (e) => {
+        setState({
             [e.target.name]: e.target.value
         })
     }
-    render() {
-        console.log(this.props)
+    const displayInputField = () => {
+        return state.map((input,i) =>{
+            return <InputEdit key={input.name} onUserEdit = {handleChange} user ={user} setUser = {setUser} input={state} i={i} setState={setState}/>
+        })
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        await actions.editProfile(this.state).then(res=>console.log(res.data))
+    }
+    
         return (
             <div>
-             
+             {displayInputField()}
             </div>
         );
-    }
 }
 
 export default EditProfile;
--- frontend/src/components/profile/InputEdit.js
+++ frontend/src/components/profile/InputEdit.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const InputEdit = (props) => {
+    const {setState, i, input, user, setUser} = props;
     return (
         <div>
             {/* Get these attr from props */}
-            {/* <input name="email" placeholder="email" onChange={props.onUserEdit} 
-            disabled={props.edit[props.name+ 'Edit']}/> */}
+            <input name={input[i].name} placeholder={input[i].name} onChange={e => {
+                let copyUser = {...user};
+                copyUser[input[i].name] = e.target.value;
+                setUser(copyUser)
+            }} 
+                disabled={input[i].disabled} value={user[i]}/>
+            <button onClick = {() => {
+                let copyState = [...input];
+                copyState[i].disabled = !input[i].disabled
+                setState(copyState)}}>{input[i].disabled ? 'Enable' : 'Disable'}</button>
         </div>
     );
 };
